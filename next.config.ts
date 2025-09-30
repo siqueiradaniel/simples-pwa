5e0bccb0b5e2dbc0044a70b7de70a1a20672832e
siqueiradaniel <danielsiqueira111@gmail.com>
2025-09-30 18:03:15 -0300

    start

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import createNextPwa from "next-pwa";
+
+const withPWA = createNextPwa({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
