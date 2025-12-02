50bf4c8951c8d6b783142fa53e2c7ad453e35c27
siqueiradaniel <danielsiqueira111@gmail.com>
2025-12-02 18:20:26 -0300

    add products icon

--- components/VerticalScrollingCategory.tsx
+++ components/VerticalScrollingCategory.tsx
@@ -11,19 +11,19 @@ const VerticalScrollingCategory = ({ title }: VerticalScrollingCategoryProps) =>
     {
       id: 1, stockQuantity: 50, initialQuantity: 100, minQuantity: 10, wantedQuantity: 80, buyPrice: 3.50, sellPrice: 23.43, profit: 3.49, totalCost: 350, totalToEarn: 699, currentEarn: 349, stockLevel: 'high', expirationDate: '2025-12-31', isAvailable: true, isPromotion: false, productId: 101, branchId: 1,
       productName: 'Arroz - Sepé Bianco Tipo 1',
-      productImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqYgG4s4Ztq9V4z0u4z4u4z4u4z4u4z4u4z4', 
+      productImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ88kamtBQWt-3eAGCV6THLyeJV7gbFSPISSKJxFI9RvQ&s', 
       brand: 'Sepé', unit: '5kg'
     },
     {
       id: 2, stockQuantity: 30, initialQuantity: 60, minQuantity: 5, wantedQuantity: 50, buyPrice: 4.20, sellPrice: 8.49, profit: 4.29, totalCost: 252, totalToEarn: 509, currentEarn: 257, stockLevel: 'medium', expirationDate: '2025-11-30', isAvailable: true, isPromotion: true, productId: 102, branchId: 1,
       productName: 'Feijão Carioca Camil',
-      productImage: 'https://placehold.co/150x150?text=Feijao', 
+      productImage: 'https://www.camil.com.br/wp-content/uploads/sites/12/2020/06/mkp-feijao-carioca-1kg-removebg-preview-300x300.png', 
       brand: 'Camil', unit: '1kg'
     },
     {
       id: 3, stockQuantity: 45, initialQuantity: 80, minQuantity: 8, wantedQuantity: 70, buyPrice: 3.80, sellPrice: 7.49, profit: 3.69, totalCost: 304, totalToEarn: 599, currentEarn: 295, stockLevel: 'high', expirationDate: '2026-01-15', isAvailable: true, isPromotion: false, productId: 103, branchId: 1,
       productName: 'Macarrão Espaguete Gallo',
-      productImage: 'https://placehold.co/150x150?text=Macarrao', 
+      productImage: 'https://www.arenaatacado.com.br/on/demandware.static/-/Sites-storefront-catalog-sv/default/dwe6328910/Produtos/37699-7896022200732-macarrao%20semola%20espaguete%20galo%20500g-galo-1.jpg', 
       brand: 'Gallo', unit: '500g'
     }
   ]
